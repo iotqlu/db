c21b766df3a1af05cdc40c7d470c7e31bb0522e6
Ming Jing <jingming.cn@gmail.com>
2021-04-01 19:16:52 +0800

    Update config.php

--- s21/config.php
+++ s21/config.php
@@ -185,11 +185,12 @@ $default_timezone = 'Asia/Shanghai';
 
 // Root path for file manager
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
-$root_path = $_SERVER['DOCUMENT_ROOT'];
+//$root_path = $_SERVER['DOCUMENT_ROOT'];
+$root_path = '/var/www/db-s21/';
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
-$root_url = '/var/www/db-s21/';
+$root_url = '';
 
 // Server hostname. Can set manually if wrong
 $http_host = $_SERVER['HTTP_HOST'];
